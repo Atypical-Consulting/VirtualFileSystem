14c9388ae47600fc6c200112e6726d6f0715af99
Philippe Matray <phmatray@gmail.com>
2023-03-16 17:07:31 +0100

    Split VFS.Queries.cs into separate operation files
    This commit splits the VFS.Queries.cs file into separate files for each operation to improve code organization and maintainability. A new folder named "Queries" has been created, and each operation now resides in its own file within this folder. This change follows best practices for .NET 6 and will make it easier to maintain and extend the code in the future.

---
+++ src/Atypical.VirtualFileSystem.Core/Queries/VFS.GetFile.cs
@@ -0,0 +1,12 @@
+﻿namespace Atypical.VirtualFileSystem.Core;
+
+public partial record VFS
+{
+    /// <inheritdoc cref="IVirtualFileSystem.GetFile(VFSFilePath)" />
+    public IFileNode GetFile(VFSFilePath filePath)
+        => (IFileNode)this.Index[filePath.Value];
+
+    /// <inheritdoc cref="IVirtualFileSystem.GetFile(string)" />
+    public IFileNode GetFile(string filePath)
+        => GetFile(new VFSFilePath(filePath));
+}
---
+++ src/Atypical.VirtualFileSystem.Core/Queries/VFS.IsEmpty.cs
@@ -0,0 +1,8 @@
+﻿namespace Atypical.VirtualFileSystem.Core;
+
+public partial record VFS
+{
+    /// <inheritdoc cref="IVirtualFileSystem.IsEmpty()" />
+    public bool IsEmpty()
+        => this.Index.Count == 0;
+}
