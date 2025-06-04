09ad055ba8dac74ac6c7ee12fbcfa3519eb09939
Philippe Matray <phmatray@gmail.com>
2025-06-04 16:07:21 +0200

    feat: improve demo pages

--- src/Atypical.VirtualFileSystem.DemoBlazorApp/Services/VFSStateService.cs
+++ src/Atypical.VirtualFileSystem.DemoBlazorApp/Services/VFSStateService.cs
@@ -5,22 +5,73 @@ namespace Atypical.VirtualFileSystem.DemoBlazorApp.Services;
 public class VFSStateService
 {
     private IVirtualFileSystemNode? _selectedNode;
+    private bool _isLoading;
+    private string? _lastErrorMessage;
     
     public IVirtualFileSystemNode? SelectedNode 
     { 
         get => _selectedNode;
         set
         {
-            _selectedNode = value;
-            OnSelectedNodeChanged?.Invoke(value);
+            if (_selectedNode != value)
+            {
+                _selectedNode = value;
+                OnSelectedNodeChanged?.Invoke(value);
+                NotifyStateChanged();
+            }
+        }
+    }
+
+    public bool IsLoading
+    {
+        get => _isLoading;
+        set
+        {
+            if (_isLoading != value)
+            {
+                _isLoading = value;
+                NotifyStateChanged();
+            }
+        }
+    }
+
+    public string? LastErrorMessage
+    {
+        get => _lastErrorMessage;
+        set
+        {
+            if (_lastErrorMessage != value)
+            {
+                _lastErrorMessage = value;
+                OnErrorChanged?.Invoke(value);
+                NotifyStateChanged();
+            }
         }
     }
 
     public event Action<IVirtualFileSystemNode?>? OnSelectedNodeChanged;
     public event Action? OnDataChanged;
+    public event Action? OnStateChanged;
+    public event Action<string?>? OnErrorChanged;
 
     public void NotifyDataChanged()
     {
         OnDataChanged?.Invoke();
+        NotifyStateChanged();
+    }
+
+    public void NotifyStateChanged()
+    {
+        OnStateChanged?.Invoke();
+    }
+
+    public void ClearError()
+    {
+        LastErrorMessage = null;
+    }
+
+    public void SetError(string errorMessage)
+    {
+        LastErrorMessage = errorMessage;
     }
 }
